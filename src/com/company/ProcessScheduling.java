d820dc112383b6b4812a29201019f9e5c37aa716
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2019-11-27 11:48:20 +0200

    Design of the project

--- src/com/company/ProcessScheduling.java
+++ src/com/company/ProcessScheduling.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class ProcessScheduling {
-    List<Process> Queue;
     InputStream inputStream;
 
     ProcessScheduling(InputStream inputStream) {
